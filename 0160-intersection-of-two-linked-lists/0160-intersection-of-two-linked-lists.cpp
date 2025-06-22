0d81ff9686379b053cbe1ca5488b5d665197b24e
indra kumar <147529343+cs23b2009@users.noreply.github.com>
2025-06-22 08:02:11 +0530

    Time: 37 ms (61.69%), Space: 18.6 MB (53.24%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -9,18 +9,40 @@
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        // Get lengths of both lists
+        int length1 = 0, length2 = 0;
         ListNode* temp1 = headA;
-        unordered_map<ListNode*, int> mpp;  // ✅ Use pointer type
+        ListNode* temp2 = headB;
+        
         while(temp1 != nullptr){
-            mpp[temp1] = 1;
+            length1++;
             temp1 = temp1->next;
         }
-        ListNode* temp2 = headB;
+        
         while(temp2 != nullptr){
-            if(mpp.find(temp2) != mpp.end())
-                return temp2;
+            length2++;
             temp2 = temp2->next;
         }
+        
+        // Reset pointers
+        temp1 = headA;
+        temp2 = headB;
+        
+        // Move the pointer of longer list forward by difference in lengths
+        int d = abs(length1 - length2);
+        if(length1 > length2){
+            while(d--) temp1 = temp1->next;
+        } else {
+            while(d--) temp2 = temp2->next;
+        }
+        
+        // Move both pointers together until intersection is found
+        while(temp1 != nullptr && temp2 != nullptr){
+            if(temp1 == temp2) return temp1;
+            temp1 = temp1->next;
+            temp2 = temp2->next;
+        }
+        
         return nullptr;
     }
 };
