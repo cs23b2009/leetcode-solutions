5a678cdb0407c93bc9784fcac345c1aba9388fe0
indra kumar <147529343+cs23b2009@users.noreply.github.com>
2025-07-09 13:22:58 +0530

    Time: 0 ms (100%), Space: 10.8 MB (70.3%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -3,20 +3,27 @@ public:
     int characterReplacement(string s, int k) {
         int n = s.length();
         int ans = 0;
-        int l = 0 ;
-        int maxi =0;
-            vector<int> freq(26,0);
-            for(int r=0;r<n;r++){
-                freq[tolower(s[r])-'a']++;
-                maxi = max(maxi,freq[tolower(s[r])-'a']) ;
-                int length = r-l+1;
-                int changes =  length - maxi;
-                if(changes>k){
-                    freq[tolower(s[l]) - 'a']--;
-                    l++;  
-                }
-                    ans = max(ans,r-l+1);
+        int l = 0;
+        int maxi = 0;
+        vector<int> freq(26, 0);
+
+        for (int r = 0; r < n; r++) {
+            freq[tolower(s[r]) - 'a']++;
+            maxi = max(maxi, freq[tolower(s[r]) - 'a']);
+
+            // Check how many characters we need to change
+            int length = r - l + 1;
+            int changes = length - maxi;
+
+            // If changes exceed k, shrink the window
+            if (changes > k) {
+                freq[tolower(s[l]) - 'a']--;
+                l++;
             }
-               return ans;
+
+            ans = max(ans, r - l + 1);
         }
+
+        return ans;
+    }
 };
