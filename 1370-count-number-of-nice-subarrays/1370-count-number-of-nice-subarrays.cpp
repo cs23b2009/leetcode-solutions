bd7b18cbd6dc6ee0673c71e8dd02a97da489666f
indra kumar <147529343+cs23b2009@users.noreply.github.com>
2025-07-09 20:06:10 +0530

    Time: 8 ms (58.84%), Space: 71.4 MB (68.91%) - LeetHub

--- 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.cpp
+++ 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.cpp
@@ -2,10 +2,9 @@ class Solution {
 public:
     int atMost(vector<int>& nums, int goal) {
         if (goal < 0) return 0;
-        int l = 0 , cnt = 0;
-        int sum = 0;
+        int l = 0 , cnt = 0, sum = 0;
         for (int r = 0; r < nums.size(); ++r) {
-            sum += (nums[r] % 2);  
+            sum += (nums[r] % 2);  // Count odd numbers
             while (sum > goal) {
                 sum -= (nums[l++] % 2);
             }
@@ -13,6 +12,7 @@ public:
         }
         return cnt;
     }
+
     int numberOfSubarrays(vector<int>& nums, int goal) {
         return atMost(nums, goal) - atMost(nums, goal - 1);
     }
