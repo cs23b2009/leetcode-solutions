72ee27197e845e716d4d0877d00ce194e6c1e061
indra kumar <147529343+cs23b2009@users.noreply.github.com>
2025-07-09 10:51:29 +0530

    Time: 41 ms (54.43%), Space: 75 MB (56.83%) - LeetHub

--- 0940-fruit-into-baskets/0940-fruit-into-baskets.cpp
+++ 0940-fruit-into-baskets/0940-fruit-into-baskets.cpp
@@ -1,32 +1,24 @@
 class Solution {
 public:
     int totalFruit(vector<int>& fruits) {
-        int n = fruits.size();
-        int maxlen = 0;
-        int l = 0;
-        int ele1 = -1;
-        int ele2 = -1;
-        for(int r = 0;r<n;r++){
-            if(ele1==-1 && ele2==-1){
-                ele1 = fruits[r];
-            }
-            if(ele1!=fruits[r]){
-                if(ele2==-1){
-                    ele2 = fruits[r];
+        unordered_map<int, int> basket;
+        int l = 0, maxlen = 0;
+
+        for (int r = 0; r < fruits.size(); ++r) {
+            basket[fruits[r]]++;
+
+            // If more than 2 types of fruits, shrink the window from the left
+            while (basket.size() > 2) {
+                basket[fruits[l]]--;
+                if (basket[fruits[l]] == 0) {
+                    basket.erase(fruits[l]);
                 }
-                else if(ele2!=fruits[r]){
-                    ele2 = fruits[r];
-                    int pointer = r-1;
-                    ele1 = fruits[r-1];
-                    while(pointer > l){
-                        if(fruits[pointer]!=ele1) break;
-                        pointer--;
-                    }
-                    l = pointer+1;
-                }
+                l++;
             }
-            maxlen = max(maxlen,r-l+1);
+
+            maxlen = max(maxlen, r - l + 1);
         }
+
         return maxlen;
     }
 };
