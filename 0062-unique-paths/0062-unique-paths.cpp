0a2bb7d52d720c928e7fbab8c41628d21e10b4e9
indra kumar <147529343+cs23b2009@users.noreply.github.com>
2025-07-11 15:18:01 +0530

    Time: 0 ms (100%), Space: 9.4 MB (33.69%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,27 +1,15 @@
 class Solution {
 public:
-    int uniquePaths(int m, int n) {
-        vector<int>prev(n,0);
-        for(int i=0;i<m;i++){
-            vector<int> temp(n,0);
-            for(int j=0;j<n;j++){
-            if(i==0 && j==0){
-                temp[j] = 1;
-                continue;
-            }
-            int up = 0;
-            int left = 0;
-            if(i>0){
-                up = prev[j];
-            }
-            if(j>0){
-                left = temp[j-1];
-            }
-            temp[j] = up + left;
-        }  prev = temp;
-        }
-        return prev[n-1];
+    int func(int i , int j ,  vector<vector<int>> &dp){
+        if(i == 0 && j == 0) return 1;
+        if(i < 0 || j < 0) return 0;
+        if(dp[i][j]!=-1) return dp[i][j];
         
+        return dp[i][j] = func(i - 1, j,dp) + func(i, j - 1,dp);
     }
 
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> dp(m,vector<int>(n,-1));
+        return func(m - 1, n - 1, dp);
+    }
 };
