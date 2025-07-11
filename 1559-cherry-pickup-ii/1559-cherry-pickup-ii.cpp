3161aa4b45f948f2e961900a87fb354e6568e81c
indra kumar <147529343+cs23b2009@users.noreply.github.com>
2025-07-11 22:54:35 +0530

    Time: 61 ms (35.02%), Space: 13.2 MB (98.51%) - LeetHub

--- 1559-cherry-pickup-ii/1559-cherry-pickup-ii.cpp
+++ 1559-cherry-pickup-ii/1559-cherry-pickup-ii.cpp
@@ -1,36 +1,51 @@
 class Solution {
 public:
-    int func(vector<vector<int>>& grid, vector<vector<vector<int>>> &dp, int i, int j1, int j2) {
-        int n = grid.size(), m = grid[0].size();
+    int maximumChocolates(int n, int m, vector<vector<int>> &grid) {
+    vector<vector<int>> front(m, vector<int>(m, 0));
+    vector<vector<int>> cur(m, vector<int>(m, 0));
 
-        // Boundary check
-        if (j1 < 0 || j1 >= m || j2 < 0 || j2 >= m) return INT_MIN;
-
-        // Check memoization
-        if (dp[i][j1][j2] != -1) return dp[i][j1][j2];
-
-        // Base case
-        if (i == n - 1) {
-            return dp[i][j1][j2] = (j1 == j2) ? grid[i][j1] : grid[i][j1] + grid[i][j2];
+    for (int j1 = 0; j1 < m; j1++) {
+        for (int j2 = 0; j2 < m; j2++) {
+            if (j1 == j2)
+                front[j1][j2] = grid[n - 1][j1];
+            else
+                front[j1][j2] = grid[n - 1][j1] + grid[n - 1][j2];
         }
-
-        int maxCherries = INT_MIN;
-        for (int dj1 = -1; dj1 <= 1; ++dj1) {
-            for (int dj2 = -1; dj2 <= 1; ++dj2) {
-                int nextJ1 = j1 + dj1;
-                int nextJ2 = j2 + dj2;
-                int result = func(grid, dp, i + 1, nextJ1, nextJ2);
-                maxCherries = max(maxCherries, result);
-            }
-        }
-
-        int currCherries = (j1 == j2) ? grid[i][j1] : grid[i][j1] + grid[i][j2];
-        return dp[i][j1][j2] = currCherries + maxCherries;
     }
 
-    int cherryPickup(vector<vector<int>>& grid) {
-        int n = grid.size(), m = grid[0].size();
-        vector<vector<vector<int>>> dp(n, vector<vector<int>>(m, vector<int>(m, -1))); // use -1 for memo
-        return func(grid, dp, 0, 0, m - 1);
+    for (int i = n - 2; i >= 0; i--) {
+        for (int j1 = 0; j1 < m; j1++) {
+            for (int j2 = 0; j2 < m; j2++) {
+                int maxi = INT_MIN;
+                for (int di = -1; di <= 1; di++) {
+                    for (int dj = -1; dj <= 1; dj++) {
+                        int ans;
+                        if (j1 == j2)
+                            ans = grid[i][j1];
+                        else
+                            ans = grid[i][j1] + grid[i][j2];
+
+                        if ((j1 + di < 0 || j1 + di >= m) || (j2 + dj < 0 || j2 + dj >= m))
+                            ans += -1e9;
+                        else
+                            ans += front[j1 + di][j2 + dj];
+
+                        maxi = max(ans, maxi);
+                    }
+                }
+                cur[j1][j2] = maxi;
+            }
+        }
+        front = cur;
+    }
+
+    return front[0][m - 1];
+}
+
+    int cherryPickup(vector<vector<int>>& matrix) {
+        int n = matrix.size(); 
+    int m = matrix[0].size(); 
+
+    return maximumChocolates(n, m, matrix);
     }
 };
