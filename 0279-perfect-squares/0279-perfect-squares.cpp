0ec8526ff1f5098b369560bd94ade5bb3c89560e
indra kumar <147529343+cs23b2009@users.noreply.github.com>
2025-07-11 12:39:23 +0530

    Time: 0 ms (100%), Space: 8.3 MB (98.27%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.cpp
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -1,21 +1,42 @@
-class Solution {
+class Solution 
+{  
+private:  
+    int is_square(int n)
+    {  
+        int sqrt_n = (int)(sqrt(n));  
+        return (sqrt_n*sqrt_n == n);  
+    }
+    
 public:
-    int func(int n , vector<int>&dp){
-        if(n==0) return 0;
-        if(dp[n]!=-1) return dp[n];
-        int minsteps = INT_MAX;
-        for(int i=1;i*i<=n;i++){
-            if(i*i<=n){
-                minsteps = min(minsteps,1 + func(n-i*i , dp));
+    int numSquares(int n) 
+    {  
+        if(is_square(n)) 
+        {
+            return 1;  
+        }
+        
+        // The result is 4 if and only if n can be written in the 
+        // form of 4^k*(8*m + 7). Please refer to 
+        // Legendre's three-square theorem.
+        while ((n & 3) == 0) // n%4 == 0  
+        {
+            n >>= 2;  
+        }
+        if ((n & 7) == 7) // n%8 == 7
+        {
+            return 4;
+        }
+        
+        // Check whether 2 is the result.
+        int sqrt_n = (int)(sqrt(n)); 
+        for(int i = 1; i <= sqrt_n; i++)
+        {  
+            if (is_square(n - i*i)) 
+            {
+                return 2;  
             }
-        }
-        return dp[n] = minsteps;
-    }
-    int numSquares(int n) {
-        for(int i=0;i*i<=n;i++){
-            if(i*i==n) return 1;
-        }
-        vector<int> dp(n+1,-1);
-        return func(n , dp);
-    }
-};
+        }  
+        
+        return 3;  
+    }  
+}; 
