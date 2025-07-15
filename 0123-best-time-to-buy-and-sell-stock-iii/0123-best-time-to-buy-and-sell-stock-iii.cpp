a58cbb912ada1da52835b45510ce8868c625a14d
indra kumar <147529343+cs23b2009@users.noreply.github.com>
2025-07-15 09:46:19 +0530

    Time: 551 ms (5.25%), Space: 222.7 MB (33.48%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,23 +1,25 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        vector<vector<vector<int>>> dp(prices.size()+1,vector<vector<int>>(2,vector<int>(3,0)));
+        vector<vector<int>> prev(2,vector<int>(3,0));
         for(int i= prices.size()-1;i>=0;i--){
+            vector<vector<int>> curr(2,vector<int>(3,0));
             for(int j=0;j<2;j++){
                 for(int k=1;k<3;k++){
                     if(j==1){
-                        int profit = -prices[i] + dp[i+1][0][k];
-                       int skip = dp[i+1][1][k];
-                        dp[i][j][k] = max(profit , skip);
+                        int profit = -prices[i] + prev[0][k];
+                       int skip = prev[1][k];
+                        curr[j][k] = max(profit , skip);
                     }
                     else{
-                        int sell = prices[i] + dp[i+1][1][k-1];
-                        int skip = dp[i+1][0][k];
-                        dp[i][j][k]=max(sell,skip);
+                        int sell = prices[i] + prev[1][k-1];
+                        int skip = prev[0][k];
+                        curr[j][k]=max(sell,skip);
                     }
                 }
             }
+            prev = curr;
         }
-        return dp[0][1][2];
+        return prev[1][2];
     }
 };
