bc40c2db39a140303fbfcde15dd2e992f42bcb89
indra kumar <147529343+cs23b2009@users.noreply.github.com>
2025-07-16 18:10:45 +0530

    Time: 87 ms (49.3%), Space: 14.5 MB (51.54%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,22 +1,39 @@
-#define ll long long
-#define pb push_back
 class Solution {
 public:
-    int lengthOfLIS(vector<int>& a) {
-        ll n = a.size();
-        vector<ll> seq;
-        vector<ll> ans;
-        for (ll i = 0; i < n; i++) {
-            auto it = lower_bound(seq.begin(), seq.end(), a[i]);
-            ll idx = it - seq.begin();
-            if (it == seq.end()) {
-                seq.pb(a[i]);
-                ans.pb(i);
-            } else {
-                seq[idx] = a[i];
-                ans[idx] = i;
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> dp(n, 1);
+        vector<int> hash(n);  // To store predecessors
+        int maxi = 1, lastIndex = 0;
+
+        for(int i = 0; i < n; i++) {
+            hash[i] = i; // initially point to self
+            for(int prev = 0; prev < i; prev++) {
+                if(nums[prev] < nums[i] && dp[prev] + 1 > dp[i]) {
+                    dp[i] = dp[prev] + 1;
+                    hash[i] = prev;
+                }
+            }
+            if(dp[i] > maxi) {
+                maxi = dp[i];
+                lastIndex = i;
             }
         }
-        return ans.size();
+
+        // Reconstruct LIS
+        vector<int> lis;
+        while(hash[lastIndex] != lastIndex) {
+            lis.push_back(nums[lastIndex]);
+            lastIndex = hash[lastIndex];
+        }
+        lis.push_back(nums[lastIndex]);
+
+        reverse(lis.begin(), lis.end());
+        for(int num : lis) {
+            cout << num << " ";
+        }
+        cout << endl;
+
+        return maxi;
     }
 };
