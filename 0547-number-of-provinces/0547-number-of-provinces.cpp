83077147c215a7a2c92ddcb052937211297b9f18
indra kumar <147529343+cs23b2009@users.noreply.github.com>
2025-12-25 19:04:00 +0530

    Time: 4 ms (28.21%), Space: 20.7 MB (33.97%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,37 +1,41 @@
 class Solution {
 public:
-    void dfs(vector<vector<int>>& adjlist, vector<int>& visited, int source) {
-        visited[source] = 1;
-        for (auto it : adjlist[source]) {
-            if (!visited[it]) {
-                dfs(adjlist, visited, it);
-            }
-        }
-    }
-
-    int findCircleNum(vector<vector<int>>& isConnected) {
-        int n = isConnected.size();
-        vector<vector<int>> adjlist(n);  // Use vector of vectors for adjacency list
-
-        // Build adjacency list from the isConnected matrix
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (isConnected[i][j] == 1 && i != j) {
-                    adjlist[i].push_back(j);
+    void bfs(vector<int>& visited,vector<int> adj[],int node){
+        queue<int> q;
+        q.push(node);
+        visited[node] = 1;
+        while(!q.empty()){
+            int ele = q.front();
+            q.pop();
+            
+            for(auto it: adj[ele]){
+                if(!visited[it]){
+                    q.push(it);
+                    visited[it] = 1;
                 }
             }
         }
-
-        vector<int> visited(n, 0);
-        int count = 0;
-
-        for (int i = 0; i < n; i++) {
-            if (!visited[i]) {
-                count++;
-                dfs(adjlist, visited, i);
+    }
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int n = isConnected.size();
+        vector<int> adj[n];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(isConnected[i][j]==1){
+                    adj[i].push_back(j);
+                }
             }
         }
-
+        int count = 0;
+        vector<int> visited(n,0);
+        for(int i=0;i<n;i++){
+            for(auto it:adj[i]){
+                if(!visited[it]){
+                    bfs(visited,adj,it);
+                    count++;
+                }
+            }
+        }
         return count;
     }
 };
