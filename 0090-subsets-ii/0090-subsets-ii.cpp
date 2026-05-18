1eb6cf5114804d7742ac9d4bdb11197026c03f86
indra kumar <147529343+cs23b2009@users.noreply.github.com>
2026-05-18 16:14:50 +0530

    Time: 0 ms (100%), Space: 10.4 MB (64.14%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
-    void backtrack(vector<int>& nums, int ind, vector<vector<int>>& ans, vector<int>& doop) {
-        ans.push_back(doop);  // Add the current subset
-
-        for (int i = ind; i < nums.size(); i++) {
-            if (i > ind && nums[i] == nums[i - 1]) continue;  // Skip duplicates
-            doop.push_back(nums[i]);
-            backtrack(nums, i + 1, ans, doop);  // Use i+1 to move to next element
-            doop.pop_back();  // Backtrack
+    void cur(int index,vector<int>& nums,vector<vector<int>>& ans,vector<int>&arr){
+            ans.push_back(arr);
+        for(int i=index;i<nums.size();i++){
+            if(i>index && nums[i]==nums[i-1]){
+                continue;
+            }
+            arr.push_back(nums[i]);
+            cur(i+1,nums,ans,arr);
+            arr.pop_back();
         }
     }
-
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        sort(nums.begin(), nums.end());  // Sort to handle duplicates
-        vector<int> doop;
+        sort(nums.begin(),nums.end());
         vector<vector<int>> ans;
-        backtrack(nums, 0, ans, doop);
+        vector<int>arr;
+        cur(0,nums,ans,arr);
         return ans;
     }
 };
