7bf993190931d0f4d2748f9bd0a82bf05529628a
indra kumar <147529343+cs23b2009@users.noreply.github.com>
2026-05-18 15:18:50 +0530

    Time: 0 ms (100%), Space: 13.9 MB (81.54%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,24 +1,25 @@
 class Solution {
 public:
-    void recursionn(int ind, vector<int>& candidates, int target, vector<vector<int>>& ans, vector<int>& arr) {
+    void recur(int index, vector<int>& candidates, int target,
+               vector<int>& arr, vector<vector<int>>& ans) {
+
         if(target == 0) {
             ans.push_back(arr);
             return;
         }
 
-        for(int i = ind; i < candidates.size(); i++) {
+        for(int i = index; i < candidates.size(); i++) {
 
-            if(i > ind && candidates[i] == candidates[i - 1]) {
+            // skip duplicates
+            if(i > index && candidates[i] == candidates[i - 1])
                 continue;
-            }
 
-            if(candidates[i] > target) {
+            if(candidates[i] > target)
                 break;
-            }
 
             arr.push_back(candidates[i]);
 
-            recursionn(i + 1, candidates, target - candidates[i], ans, arr);
+            recur(i + 1, candidates, target - candidates[i], arr, ans);
 
             arr.pop_back();
         }
@@ -31,7 +32,7 @@ public:
         vector<vector<int>> ans;
         vector<int> arr;
 
-        recursionn(0, candidates, target, ans, arr);
+        recur(0, candidates, target, arr, ans);
 
         return ans;
     }
