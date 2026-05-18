95fe707f8908fe608ada93786e776884408ff4b3
indra kumar <147529343+cs23b2009@users.noreply.github.com>
2026-05-18 12:38:20 +0530

    Time: 0 ms (100%), Space: 14.1 MB (52.15%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,25 +1,26 @@
 class Solution {
 public:
-    void backtrack(int ind, vector<int>& candidates, int target, vector<int>& curr, vector<vector<int>>& ans) {
-        if (target == 0) {
-            ans.push_back(curr);
+    void recursionn(int ind,vector<int>& candidates,int target,vector<vector<int>>& ans,vector<int>&arr){
+        if(target==0){
+            ans.push_back(arr);
             return;
         }
-        if (ind >= candidates.size()) return;
-
-        if (candidates[ind] <= target) {
-            curr.push_back(candidates[ind]);
-            backtrack(ind, candidates, target - candidates[ind], curr, ans); // allow reuse
-            curr.pop_back();
+        if(ind>=candidates.size()){
+            return;
         }
-
-        backtrack(ind + 1, candidates, target, curr, ans); // skip current
+        if(candidates[ind]<=target){
+            arr.push_back(candidates[ind]);
+            recursionn(ind,candidates,target-candidates[ind],ans,arr);
+            arr.pop_back();
+        }
+           recursionn(ind+1,candidates,target,ans,arr); 
     }
 
+   
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        vector<vector<int>> ans;
-        vector<int> curr;
-        backtrack(0, candidates, target, curr, ans);
+        vector<vector<int>>ans;
+        vector<int>arr;
+        recursionn(0,candidates,target,ans,arr); 
         return ans;
     }
 };
