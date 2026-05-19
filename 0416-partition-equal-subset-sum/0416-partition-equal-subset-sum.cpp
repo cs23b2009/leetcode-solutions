777f3472cf58972d57346513698641d0fdae88c0
indra kumar <147529343+cs23b2009@users.noreply.github.com>
2026-05-19 11:48:18 +0530

    Time: 159 ms (27.93%), Space: 110.6 MB (23.17%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,20 +1,32 @@
 class Solution {
 public:
-    bool canPartition(vector<int>& nums) {
-        int total = accumulate(nums.begin(), nums.end(), 0);
-
-        if (total % 2 != 0) return false;
-        
-        int target = total / 2;
-        vector<bool> dp(target + 1, false);
-        dp[0] = true;
-
-        for (int num : nums) {
-            for (int j = target; j >= num; j--) {
-                dp[j] = dp[j] || dp[j - num];
-            }
+    bool recur(int index,vector<int>&nums,int sum,vector<vector<int>>&dp){
+        if(sum==0){
+            return true;
         }
-
-        return dp[target];
+        if(index==nums.size()){
+            return false;
+        }
+        if(dp[index][sum]!=-1) return dp[index][sum];
+        bool pick = false;
+        if(nums[index]<=sum){
+            pick = recur(index+1,nums,sum-nums[index],dp);
+        }
+        bool nonpick = recur(index+1,nums,sum,dp);
+        return dp[index][sum] = pick||nonpick;
+    }
+    bool canPartition(vector<int>& nums) {
+        int sum = 0;
+        for(int i=0;i<nums.size();i++){
+            sum += nums[i];
+        }
+        if(sum%2==0){
+            sum /= 2;
+        }
+        else{
+            return false;
+        }
+        vector<vector<int>>dp(nums.size(),vector<int>(sum+1,-1));
+        return recur(0,nums,sum,dp);
     }
 };
