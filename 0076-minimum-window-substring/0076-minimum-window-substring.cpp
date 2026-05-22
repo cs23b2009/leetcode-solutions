536dbd71c9723e70aa12ebfb6903ee7722ac51d2
indra kumar <147529343+cs23b2009@users.noreply.github.com>
2026-05-22 12:34:06 +0530

    Time: 0 ms (100%), Space: 11.5 MB (62.49%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,42 +1,48 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        if (t.empty() || s.empty()) return "";
 
-        unordered_map<char, int> tFreq;
-        for (char ch : t) tFreq[ch]++;
+        vector<int> freq(256, 0);
 
-        int required = tFreq.size(); 
-        int formed = 0;
-        unordered_map<char, int> windowFreq;
-
-        int l = 0, r = 0;
-        int minLen = INT_MAX, startIdx = 0;
-
-        while (r < s.size()) {
-            char c = s[r];
-            windowFreq[c]++;
-
-            if (tFreq.count(c) && windowFreq[c] == tFreq[c]) {
-                formed++;
-            }
-
-            while (l <= r && formed == required) {
-                if (r - l + 1 < minLen) {
-                    minLen = r - l + 1;
-                    startIdx = l;
-                }
-
-                windowFreq[s[l]]--;
-                if (tFreq.count(s[l]) && windowFreq[s[l]] < tFreq[s[l]]) {
-                    formed--;
-                }
-                l++;
-            }
-
-            r++;
+        for(char ch : t) {
+            freq[ch]++;
         }
 
-        return (minLen == INT_MAX) ? "" : s.substr(startIdx, minLen);
+        int left = 0;
+        int count = 0;
+
+        int minLen = INT_MAX;
+        int start = 0;
+
+        for(int right = 0; right < s.size(); right++) {
+
+            if(freq[s[right]] > 0) {
+                count++;
+            }
+
+            freq[s[right]]--;
+
+            while(count == t.size()) {
+
+                if(right - left + 1 < minLen) {
+                    minLen = right - left + 1;
+                    start = left;
+                }
+
+                freq[s[left]]++;
+
+                if(freq[s[left]] > 0) {
+                    count--;
+                }
+
+                left++;
+            }
+        }
+
+        if(minLen == INT_MAX) {
+            return "";
+        }
+
+        return s.substr(start, minLen);
     }
 };
