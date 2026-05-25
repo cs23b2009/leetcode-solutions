c07abbacc71b1eab84482f8928e02be916933878
indra kumar <147529343+cs23b2009@users.noreply.github.com>
2026-05-25 19:33:05 +0530

    Time: 33 ms (19.47%), Space: 85.7 MB (39.35%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -1,40 +1,56 @@
 class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
+
         int n = heights.size();
-        vector<int> nse(n);
         vector<int> pse(n);
+        vector<int> nse(n);
+
         stack<int> st;
+        for (int i = 0; i < n; i++) {
 
-        // Compute NSE (Next Smaller Element)
-        for(int i = n - 1; i >= 0; i--) {
-            while(!st.empty() && heights[st.top()] >= heights[i]) {
+            while (!st.empty() && heights[st.top()] >= heights[i]) {
                 st.pop();
             }
-            nse[i] = st.empty() ? n : st.top();
+
+            if (st.empty()) {
+                pse[i] = -1;
+            }
+            else {
+                pse[i] = st.top();
+            }
+
             st.push(i);
         }
 
-        // Clear the stack before reusing
-        while(!st.empty()) st.pop();
+        while (!st.empty()) st.pop();
+        for (int i = n - 1; i >= 0; i--) {
 
-        // Compute PSE (Previous Smaller Element)
-        for(int i = 0; i < n; i++) {
-            while(!st.empty() && heights[st.top()] >= heights[i]) {
+            while (!st.empty() && heights[st.top()] >= heights[i]) {
                 st.pop();
             }
-            pse[i] = st.empty() ? -1 : st.top();
+
+            if (st.empty()) {
+                nse[i] = n;
+            }
+            else {
+                nse[i] = st.top();
+            }
+
             st.push(i);
         }
 
-        // Calculate max area
-        int maxArea = 0;
-        for(int i = 0; i < n; i++) {
+        int maxi = 0;
+
+        for (int i = 0; i < n; i++) {
+
             int width = nse[i] - pse[i] - 1;
-            int area = width * heights[i];
-            maxArea = max(maxArea, area);
+
+            int area = heights[i] * width;
+
+            maxi = max(maxi, area);
         }
 
-        return maxArea;
+        return maxi;
     }
 };
