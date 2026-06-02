920f7b78ea872d3516139c0d264800ff1b57fb9c
indra kumar <147529343+cs23b2009@users.noreply.github.com>
2026-06-02 18:00:48 +0530

    Time: 33 ms (20.95%), Space: 19.5 MB (34.89%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,34 +1,35 @@
 class Solution {
 public:
-    void dfs(vector<vector<char>>& grid, vector<vector<int>>& visited, int i, int j) {
-        int m = grid.size();
-        int n = grid[0].size();
-        if(i < 0 || j < 0 || i >= m || j >= n || visited[i][j] || grid[i][j] != '1')
-            return;
-
-        visited[i][j] = 1;
-        dfs(grid, visited, i + 1, j);
-        dfs(grid, visited, i - 1, j);
-        dfs(grid, visited, i, j + 1);
-        dfs(grid, visited, i, j - 1);
-    }
-
     int numIslands(vector<vector<char>>& grid) {
-        int m = grid.size();
-        if(m == 0) return 0;
-        int n = grid[0].size();
-
-        vector<vector<int>> visited(m, vector<int>(n, 0));
-        int count = 0;
-
-        for(int i = 0; i < m; ++i) {
-            for(int j = 0; j < n; ++j) {
-                if(!visited[i][j] && grid[i][j] == '1') {
-                    ++count;
-                    dfs(grid, visited, i, j);
+        int n = grid.size();
+        int m = grid[0].size();
+        queue<pair<int,int>>q;
+        vector<vector<int>> visited(n,vector<int>(m,0));
+        vector<int> roww = {-1,0,1,0};
+        vector<int> coll = {0,1,0,-1};
+        int ans = 0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(visited[i][j]==0 && grid[i][j] =='1'){
+                    ans++;
+                    q.push({i,j});
+                    while(!q.empty()){
+                        auto it = q.front();
+                        q.pop();
+                        for(int i=0;i<4;i++){
+                            int nr = it.first +roww[i];
+                            int nc = it.second +coll[i];
+                            if(nr>=0 && nr < n && nc>=0 && nc<m){
+                                if(visited[nr][nc]==0 && grid[nr][nc] =='1'){
+                                   visited[nr][nc] = 1;
+                                   q.push({nr,nc}); 
+                                }
+                            }
+                        }
+                    }
                 }
             }
         }
-        return count;
+        return ans;
     }
 };
