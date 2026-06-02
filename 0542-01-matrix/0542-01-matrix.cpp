32583345f7db234a5e0f3109e884dd99df6bdd70
indra kumar <147529343+cs23b2009@users.noreply.github.com>
2026-06-02 18:29:30 +0530

    Time: 31 ms (20.2%), Space: 37.4 MB (49.12%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -1,41 +1,36 @@
 class Solution {
 public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-    int n = mat.size();
-    int m = mat[0].size();
-    for(int i=0;i<n;i++){
+       int n = mat.size();
+       int m = mat[0].size();
+       vector<vector<int>> visited(n,vector<int>(m,0));
+       vector<vector<int>> distance(n,vector<int>(m,0));
+       vector<int> roww = {-1,0,1,0};
+       vector<int> coll = {0,1,0,-1};
+       queue<pair<int,int>> q;
+       for(int i=0;i<n;i++){
         for(int j=0;j<m;j++){
-            mat[i][j] = !mat[i][j];
-        }
-    }
-    vector<vector<int>> visited(n,(vector<int>(m,0)));
-    vector<vector<int>> distance(n,(vector<int>(m,0)));
-    queue<pair<pair<int,int>,int>> q;
-    for(int i=0;i<n;i++){
-        for(int j=0;j<m;j++){
-            if(mat[i][j]){
-                visited[i][j] = 1;
-                q.push({{i,j},0});
+            if(mat[i][j]==0){
+                visited[i][j]=1;
+                q.push({i,j});
             }
         }
-    }
-    int dr[4] = {-1,0,1,0};
-    int dc[4] = {0,1,0,-1};
-    while(!q.empty()){
-        int node1 = q.front().first.first;
-        int node2 = q.front().first.second;
-        int dis =  q.front().second;
+       }
+       while(!q.empty()){
+        auto it = q.front();
         q.pop();
-        distance[node1][node2] = dis;
         for(int i=0;i<4;i++){
-            int nrow = node1 + dr[i];
-            int ncol = node2 + dc[i];
-            if(nrow>=0 && ncol >=0 && nrow <n && ncol <m && visited[nrow][ncol]==0){
-                q.push({{nrow,ncol},dis+1});
-                visited[nrow][ncol] = 1;
+            int nr = it.first + roww[i];
+            int nc = it.second + coll[i];
+            if(nr>=0 && nr < n && nc>=0 && nc<m){
+                if(!visited[nr][nc]){
+                    visited[nr][nc] = 1;
+                    distance[nr][nc] = distance[it.first][it.second] + 1;
+                    q.push({nr,nc});
+                }
             }
         }
-    }
-    return distance;
+       }
+       return distance;
     }
 };
