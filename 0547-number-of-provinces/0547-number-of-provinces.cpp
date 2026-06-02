ab943dde77e750b8c97033a1a25bb9865ffbd5ff
indra kumar <147529343+cs23b2009@users.noreply.github.com>
2026-06-02 16:36:06 +0530

    Time: 1 ms (46.93%), Space: 20.2 MB (42.48%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,43 +1,35 @@
 class Solution {
 public:
-    void bfs(vector<int>& visited, vector<vector<int>>& adj, int node){
-        queue<int> q;
-        visited[node] = 1;
-        q.push(node);
-
-        while(!q.empty()){
-            int ele = q.front();
-            q.pop();
-            for(auto it : adj[ele]){
-                if(!visited[it]){
-                    visited[it] = 1;
-                    q.push(it);
-                }
-            }
-        }
-    }
-
     int findCircleNum(vector<vector<int>>& isConnected) {
         int n = isConnected.size();
-        vector<vector<int>> adj(n);
-
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < n; j++){
-                if(isConnected[i][j] == 1 && i != j){
+        vector<int>adj[n];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(isConnected[i][j]==1 && i!=j){
                     adj[i].push_back(j);
                 }
             }
         }
-
-        vector<int> visited(n, 0);
-        int count = 0;
-
-        for(int i = 0; i < n; i++){
+        queue<int> q;
+        vector<int> visited(n,0);
+        int ans = 0;
+        for(int i=0;i<n;i++){
             if(!visited[i]){
-                bfs(visited, adj, i);
-                count++;
+                ans++;
+                visited[i] = 1;
+                q.push(i);
+                while(!q.empty()){
+                    int node = q.front();
+                    q.pop();
+                    for(auto it:adj[node]){
+                        if(!visited[it]){
+                            visited[it] = 1;
+                            q.push(it);
+                        }
+                    }
+                }
             }
         }
-        return count;
+        return ans;
     }
 };
