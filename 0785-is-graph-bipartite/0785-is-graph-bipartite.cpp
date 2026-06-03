1a0e1691e9f3d72e004aa2dea3280b8e0d63a1d6
indra kumar <147529343+cs23b2009@users.noreply.github.com>
2026-06-03 11:26:14 +0530

    Time: 0 ms (100%), Space: 17.5 MB (41.35%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
@@ -1,35 +1,32 @@
 class Solution {
 public:
-    bool isBipartite(vector<vector<int>>& graph) {
-        int n = graph.size();
-        vector<int> visited(n,0);
-        vector<int> colors(n,0);
-        queue<pair<int,int>> q;
-        for(int i=0;i<n;i++){
-            if(!visited[i]){
-                q.push({i,0});
-        visited[i] = 1;
-        colors[i] = 0;
-        while(!q.empty()){
-            auto it = q.front();
-            q.pop();
-            int node = it.first;
-            int color = it.second;
-            for(auto neig : graph[node]){
-                if(!visited[neig]){
-                    visited[neig] = 1;
-                    colors[neig] = !color;
-                    q.push({neig,!color});
-                }
-                else{
-                    if(colors[neig]==color){
-                        return false;
-                    }
+    bool dfs(vector<vector<int>>& graph,vector<int>& visited,vector<int>&colors,int node,int color){
+            visited[node] = 1;
+            colors[node] = color;
+        for(auto it :graph[node]){
+            if(!visited[it]){
+                if(!dfs(graph,visited,colors,it,!color)){
+                    return false;
                 }
             }
-        }
+            else{
+                if(colors[it]==color){
+                    return false;
+                }
             }
         }
         return true;
+    }
+    bool isBipartite(vector<vector<int>>& graph) {
+        int n  = graph.size();
+        vector<int> visited(n,0);
+        vector<int> colors(n,0);
+        bool ans = true;
+        for(int i=0;i<n;i++){
+            if(!visited[i]){
+                ans = ans && dfs(graph,visited,colors,i,0);
+            }
+        }
+        return ans;
     }
 };
