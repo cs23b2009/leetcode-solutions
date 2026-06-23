bc1905ebbde315f3272a2c13f9ccb47ea75f1051
indra kumar <147529343+cs23b2009@users.noreply.github.com>
2026-06-23 17:22:20 +0530

    Time: 0 ms (100%), Space: 13.5 MB (24.32%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,24 +1,21 @@
 class Solution {
 public:
-    int func(vector<vector<int>>& triangle,int row,int col,vector<vector<int>>&dp){
-        if(row<0 || col<0 || col>row) return 1e9;
-        if(row==0 && col==0) return triangle[row][col];
-        if(dp[row][col]!=-1) return dp[row][col];
-        int up = 1e9;
-        int upright = 1e9;
-        if(row>0 && col>0){
-            upright = func(triangle,row-1,col-1,dp);
-        }
-        if(row>0){
-            up = func(triangle,row-1,col,dp);
-        }
-        return  dp[row][col]=triangle[row][col]+min(upright,up);
-    }
     int minimumTotal(vector<vector<int>>& triangle) {
+        int n = triangle.size();
         int ans = INT_MAX;
-        vector<vector<int>>dp(triangle.size(),vector<int>(triangle.size(),-1));
-        for(int i=0;i<triangle.size();i++){
-            ans = min(ans,func(triangle,triangle.size()-1,i,dp));
+        vector<vector<int>>dp(n,vector<int>(n,1e9));
+        dp[0][0] = triangle[0][0];
+        for(int i=1;i<n;i++){
+            for(int j=0;j<=i;j++){
+                int up = 1e9;
+                int upright = 1e9;
+                if(i>0 && j>0){ upright = dp[i-1][j-1]; }
+                if(i>0){ up = dp[i-1][j]; }
+                dp[i][j] = triangle[i][j] + min(up,upright);
+            }
+        }
+        for(int i=0;i<n;i++){
+            ans = min(ans,dp[n-1][i]);
         }
         return ans;
     }
