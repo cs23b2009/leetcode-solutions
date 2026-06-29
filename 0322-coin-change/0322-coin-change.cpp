688e177d5bc26816945a4a1afb3fc2e509d4be44
indra kumar <147529343+cs23b2009@users.noreply.github.com>
2026-06-29 13:08:36 +0530

    Time: 20 ms (73.32%), Space: 17.8 MB (94.09%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,28 +1,15 @@
 class Solution {
 public:
-    int func(vector<int>& coins, int amount,vector<int> &dp) {
-        if (amount == 0)
-            return 0;
-
-        if (amount < 0)
-            return INT_MAX;
-
-        int mini = INT_MAX;
-        if(dp[amount]!=-1) return dp[amount];
-        for (int i = 0; i < coins.size(); i++) {
-            int res = func(coins, amount - coins[i],dp);
-
-            if (res != INT_MAX){
-                    mini = min(mini, 1 + res);
+    int coinChange(vector<int>& coins, int amount) {
+        vector<int> dp(amount + 1, INT_MAX);
+        dp[0] = 0;
+        for (int i = 1; i <= amount; i++) {
+            for (int coin : coins) {
+                if (i >= coin && dp[i - coin] != INT_MAX) {
+                    dp[i] = min(dp[i], dp[i - coin] + 1);
+                }
             }
         }
-
-        return dp[amount]=mini;
-    }
-
-    int coinChange(vector<int>& coins, int amount) {
-        vector<int> dp(amount+1,-1);
-        int ans = func(coins, amount,dp);
-        return ans == INT_MAX ? -1 : ans;
+        return dp[amount] == INT_MAX ? -1 : dp[amount];
     }
 };
