3a69a103417c3eeec20829e974551eaa58d4f0f5
indra kumar <147529343+cs23b2009@users.noreply.github.com>
2026-06-29 21:38:20 +0530

    Time: 85 ms (43.53%), Space: 76.1 MB (21.72%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
-    int longestPalindromeSubseq(string text1) {
-        string text2 = text1;
-        reverse(text2.begin(),text2.end());
-        int n = text1.size();
-        int m = text2.size();
-        vector<int> prev(m + 1, 0);
-
-        for (int i = n - 1; i >= 0; i--) {
-            vector<int> curr(m + 1, 0);
-            for (int j = m - 1; j >= 0; j--) {
-                if (text1[i] == text2[j]) {
-                    curr[j] = 1 + prev[j + 1];
-                } else {
-                    curr[j] = max(prev[j], curr[j + 1]);
-                }
-            }
-            prev = curr;
+    int func(int n , int m,string& s1,string& s2,vector<vector<int>>& dp){
+        if(n<0 || m<0) return 0;
+        if(dp[n][m]!=-1) return dp[n][m];
+        if(s1[n]==s2[m]) {
+            return dp[n][m] = 1 + func(n-1,m-1,s1,s2,dp);
         }
-        return prev[0];
+        else{
+            return dp[n][m]=max(func(n-1,m,s1,s2,dp),func(n,m-1,s1,s2,dp));
+        }
+    }
+    int longestPalindromeSubseq(string s) {
+        int n = s.size();
+        string s1 = s;
+        string s2 = "";
+        for(int i=n-1;i>=0;i--){
+            s2 += s1[i];
+        }
+        vector<vector<int>> dp(n,vector<int>(n,-1));
+        return func(n-1,n-1,s1,s2,dp);
     }
 };
