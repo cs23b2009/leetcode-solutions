1055e101f7f50f719a8ef9acf677ff3021a985e6
indra kumar <147529343+cs23b2009@users.noreply.github.com>
2026-06-29 21:47:34 +0530

    Time: 83 ms (47.48%), Space: 75.3 MB (77.84%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
@@ -7,13 +7,15 @@ public:
         for(int i=n-1;i>=0;i--){
             s2 += s1[i];
         }
-        vector<vector<int>> dp(n+1,vector<int>(n+1,0));
+        vector<int> prev(n+1,0);
         for(int i=1;i<=n;i++){
+            vector<int> curr(n+1,0);
             for(int j=1;j<=n;j++){
-                if(s1[i-1]==s2[j-1]) dp[i][j] = 1 + dp[i-1][j-1];
-                else dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+                if(s1[i-1]==s2[j-1]) curr[j] = 1 + prev[j-1];
+                else curr[j] = max(prev[j],curr[j-1]);
             }
+            prev = curr;
         }
-        return dp[n][n];
+        return prev[n];
     }
 };
